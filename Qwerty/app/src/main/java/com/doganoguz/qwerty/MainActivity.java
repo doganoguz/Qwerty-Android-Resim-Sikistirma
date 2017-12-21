a3ec994ed1f4d7d71a90d8de434e3c0bb8be33ef
ouzdogan <oguz-dogan1997@hotmail.com>
2017-12-21 04:04:45 +0300

    Qwerty
    Yüksek boyutlu resimlerinizi sıkıştırmanıza yarar.

---
+++ Qwerty/app/src/main/java/com/doganoguz/qwerty/MainActivity.java
@@ -0,0 +1,215 @@
+package com.doganoguz.qwerty;
+
+
+/**
+ * Created on : Dec 21, 2017
+ * Author     : doganoguz
+ * Name       : Doğan Oğuz
+ * GitHub     : https://github.com/doganouz
+ */
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Random;
+
+import id.zelory.compressor.Compressor;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int PICK_IMAGE_REQUEST = 1;
+
+    private ImageView actualImageView;
+    private ImageView compressedImageView;
+    private TextView actualSizeTextView;
+    private TextView compressedSizeTextView;
+    private File actualImage;
+    private File compressedImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actualImageView = (ImageView) findViewById(R.id.actual_image);
+        compressedImageView = (ImageView) findViewById(R.id.compressed_image);
+        actualSizeTextView = (TextView) findViewById(R.id.actual_size);
+        compressedSizeTextView = (TextView) findViewById(R.id.compressed_size);
+
+        actualImageView.setBackgroundColor(getRandomColor());
+        clearImage();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, PICK_IMAGE_REQUEST);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public void compressImage(View view) {
+        if (actualImage == null) {
+            showError("Lütfen Resim Seçiniz!");
+        } else {
+
+
+            new Compressor(this)
+                    .compressToFileAsFlowable(actualImage)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Consumer<File>() {
+                        @Override
+                        public void accept(File file) {
+                            compressedImage = file;
+                            setCompressedImage();
+                        }
+                    }, new Consumer<Throwable>() {
+                        @Override
+                        public void accept(Throwable throwable) {
+                            throwable.printStackTrace();
+                            showError(throwable.getMessage());
+                        }
+                    });
+        }
+    }
+    public void customCompressImage(View view) {
+        if (actualImage == null) {
+            showError("Lütfen Resim Seçiniz!");
+        } else {
+
+            try {
+                compressedImage = new Compressor(this)
+                        .setMaxWidth(640)
+                        .setMaxHeight(480)
+                        .setQuality(75)
+                        .setCompressFormat(Bitmap.CompressFormat.WEBP)
+                        .setDestinationDirectoryPath(Environment.getExternalStoragePublicDirectory(
+                                Environment.DIRECTORY_PICTURES).getAbsolutePath())
+                        .compressToFile(actualImage);
+
+                setCompressedImage();
+            } catch (IOException e) {
+                e.printStackTrace();
+                showError(e.getMessage());
+            }
+
+
+        }
+    }
+    private void setCompressedImage() {
+        compressedImageView.setImageBitmap(BitmapFactory.decodeFile(compressedImage.getAbsolutePath()));
+        compressedSizeTextView.setText(String.format("Boyut : %s", getReadableFileSize(compressedImage.length())));
+
+        Toast.makeText(this, "Resim Başarıyla Sıkıştırıldı!" + compressedImage.getPath(), Toast.LENGTH_LONG).show();
+        Log.d("Sıkıştırma", "Sıkıştırılmış Resim Yolu: " + compressedImage.getPath());
+    }
+
+    private void clearImage() {
+        actualImageView.setBackgroundColor(getRandomColor());
+        compressedImageView.setImageDrawable(null);
+        compressedImageView.setBackgroundColor(getRandomColor());
+        compressedSizeTextView.setText("Boyut : -");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK) {
+            if (data == null) {
+                showError("Oops! Resim Açılamadı!");
+                return;
+            }
+            try {
+                actualImage = FileUtil.from(this, data.getData());
+                actualImageView.setImageBitmap(BitmapFactory.decodeFile(actualImage.getAbsolutePath()));
+                actualSizeTextView.setText(String.format("Boyut : %s", getReadableFileSize(actualImage.length())));
+                clearImage();
+            } catch (IOException e) {
+                showError("\n" +
+                        "Resim verileri okunamadı!");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void showError(String errorMessage) {
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    private int getRandomColor() {
+        Random rand = new Random();
+        return Color.argb(100, rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+    }
+
+    public String getReadableFileSize(long size) {
+        if (size <= 0) {
+            return "0";
+        }
+        final String[] units = new String[]{"B", "KB", "MB", "GB", "TB"};
+        int digitGroups = (int) (Math.log10(size) / Math.log10(2048));
+        return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+
+        if (id == R.id.action_settings) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Qwerty");
+            builder.setMessage("Daha fazlası için uygulamalarıma gidebilirsiniz.");
+
+
+
+            builder.setPositiveButton("Git", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    String url = "https://play.google.com/store/apps/dev?id=8686341603030191209";
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+
+                }
+            });
+
+
+            builder.show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
